5a4633fdf75fe83dab0c8c145bf909160ce37aae
TienLe <trantiendev@gmail.com>
2022-12-22 03:05:22 +0700

    header && banner cmp && getserver data props index page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [isScrolled, setIsScrolled] = useState(false);
 
-export default Header
+  useEffect(() => {
+    const handleScrollHeader = () => (window.scrollY > 0) ? setIsScrolled(true) : setIsScrolled(false);
+
+    window.addEventListener('scroll', handleScrollHeader);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollHeader)
+    }
+  }, [])
+
+  return (
+    <header className={`${isScrolled ? 'bg-[#141414]' : ''}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink cursor-default font-semibold text-white hover:text-white">
+            Home
+          </li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+            alt="avatar"
+            width={25}
+            height={25}
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
